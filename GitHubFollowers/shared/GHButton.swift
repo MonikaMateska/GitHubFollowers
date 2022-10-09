25326a625e528befc70120216d22d97773fd24b7
Monika Mateska <monika.mateska@netcetera.com>
2022-10-09 16:25:10 +0200

    custom UIButton and UITextField

---
+++ GitHubFollowers/shared/GHButton.swift
@@ -0,0 +1,35 @@
+//
+//  GHButton.swift
+//  GitHubFollowers
+//
+//  Created by Monika Mateska on 9.10.22.
+//
+
+import UIKit
+
+class GHButton: UIButton {
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
